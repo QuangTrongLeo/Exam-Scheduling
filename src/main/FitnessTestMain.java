bf78a24a355d5cb7273293a67f80b2964ca0de59
nTruong257 <22130306@st.hcmuaf.edu.vn>
2025-11-21 09:13:45 +0700

    fix: test main

--- src/main/FitnessTestMain.java
+++ src/main/FitnessTestMain.java
@@ -10,12 +10,14 @@ public class FitnessTestMain {
     public static void main(String[] args) {
     	ScheduleController controller = new ScheduleController();
     	SchedulePrint printer = new SchedulePrint();
-    	InitPopulationService initPopulationService = new InitPopulationService();
-    	Individual individual = initPopulationService.createIndividual();
+    	
+    	// Tạo 1 cá thể để test
+        Individual individual = new InitPopulationService().createIndividual();
     	
     	
     	double fitness = controller.fitness(individual);        
         printer.printIndividual(individual);
         System.out.println("Fitness của cá thể này: " + fitness);
+        
     }
 }
